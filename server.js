8801c68b6091fc8970593c8abeb1ff8dcf14d09c
Sergej <sergej.kubat18@gmail.com>
2022-11-15 04:18:27 +0100

    Basic scaffholding 2

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const morgan = require("morgan");
+
+require("dotenv").config();
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+// MIDDLEWARES
+
+app.use(cors());
+app.use(morgan("combined"));
+
+// DATABASES
+
+mongoose.connect(process.env.MONGODB_URI);
+
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+    console.log("Database connection established successfully!");
+});
+
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
