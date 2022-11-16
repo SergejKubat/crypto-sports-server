f4cf086363132a421c7ee37c4e5aae1e1d9f5ad9
Sergej <sergej.kubat18@gmail.com>
2022-11-16 08:47:27 +0100

    Adding support for session management

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require("express");
+const session = require("express-session");
 const mongoose = require("mongoose");
+const redis = require("redis");
+const connectRedis = require("connect-redis");
 const cors = require("cors");
 const morgan = require("morgan");
 
 require("dotenv").config();
 
+const PORT = process.env.PORT || 5000;
+const ONE_DAY = 1000 * 60 * 60 * 24;
+
 const app = express();
-
-// MIDDLEWARES
-
-app.use(cors());
-app.use(morgan("combined"));
 
 // DATABASES
 
@@ -18,7 +19,36 @@ mongoose.connect(process.env.MONGODB_URI, () => {
     console.log("Database connection established successfully!");
 });
 
-const PORT = process.env.PORT || 5000;
+const RedisStore = connectRedis(session);
+
+const redisClient = redis.createClient({
+    host: "localhost",
+    port: 6379,
+});
+
+redisClient.on("error", function (err) {
+    console.log("Could not establish a connection with redis. " + err);
+});
+
+redisClient.on("connect", function (err) {
+    console.log("Connected to redis successfully");
+});
+
+// MIDDLEWARES
+
+app.use(cors());
+
+app.use(
+    session({
+        store: new RedisStore({ client: redisClient }),
+        secret: process.env.SESSION_SECRET,
+        saveUninitialized: false,
+        resave: false,
+        cookie: { secure: false, maxAge: ONE_DAY },
+    })
+);
+
+app.use(morgan("combined"));
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
